f9cf653af6f3be80087eee06fcb860d4adfae114
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-01 09:41:10 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program1854.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Runtime.Intrinsics.Arm;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1854();
+            Welcome6688();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6688();
+
+        /// <summary>
+        /// Yedidia's welcome method
+        /// </summary>
+        private static void Welcome1854()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+            Console.ReadKey();
+
+        }
+    }
+}
