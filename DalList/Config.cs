d2670e0305976a24fdeb6b72eaf2c75aaf51ae1d
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-07 16:46:27 +0200

    Call id generator and RiskRange handler has been implemented in the config class

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -1,7 +1,20 @@
 ﻿
 namespace Dal;
 
-internal static  class Config
+internal static class Config
 {
-    
+    //Id generator for the Call model
+    internal const int startCallId = 1;
+    private static int nextCallId = startCallId;
+    internal static int NextCallId { get => nextCallId++; }
+
+
+    //RiskRange handler
+    internal static TimeSpan RiskRange { get; set; } = TimeSpan.Zero;
+
+    internal static void Reset()
+    {
+        nextCallId = startCallId;
+        RiskRange = TimeSpan.Zero;
+    }
 }
