9ed7de6c696315084748bd06ddc56b029c674b5b
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-07 16:14:49 +0200

    Call record has been added along with the CallTypes enum.

---
+++ DalFacade/DO/Call.cs
@@ -0,0 +1,28 @@
+﻿namespace DO;
+/// <summary>
+/// This record entity represent the call request that has created by the manager and given to the volunteers to handle it.
+/// </summary>
+/// <param name="Id">Identifier for each call. Created automatically - provide 0 argument to initialize the property</param>
+/// <param name="Type">The type of opened call</param>
+/// <param name="FullAddressCall">The address which the call is referring to</param>
+/// <param name="Latitude">The latitude which is related to the address that is related to the call - updated by the logical layer anytime that the address is updated</param>
+/// <param name="Longitude">The longitude which is related to the address that is related to the call - updated by the logical layer anytime that the address is updated</param>
+/// <param name="OpeningTime">The time which the call has been opened by the manager</param>
+/// <param name="Description">[Optional] A description about the call</param>
+/// <param name="DeadLine">[Optional] Last time for closing the call.
+/// Given to some of the calls and used by the logical layer in order to define and calculated Risk Time Range in order to determinate whether the call is riscky or not
+/// </param>
+public record Call
+(
+    int Id,
+    CallTypes Type,
+    string FullAddressCall,
+    double Latitude,
+    double Longitude,
+    DateTime OpeningTime,
+    string? Description = null,
+    DateTime? DeadLine = null
+)
+{
+    public Call() : this(0, CallTypes.Undefined, "", 0.0, 0.0, DateTime.Now) { }
+};
--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,1 +1,2 @@
 ﻿namespace DO;
+public enum CallTypes {Undefined,FoodPreparation,FoodDelivery}
