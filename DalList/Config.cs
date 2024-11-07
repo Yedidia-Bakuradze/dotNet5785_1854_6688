f4b22344ca2b32d6128cf611bf0bc2cc025f7a8c
Meir Crombie <meircrombie@gmail.com>
2024-11-07 16:42:32 +0200

    Genrated id for the assignment model as been implemented

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -1,7 +1,17 @@
 ﻿
 namespace Dal;
 
-internal static  class Config
+internal static class Config
 {
-    
+    internal const int startAssignmentId = 0;
+    private static int nextAssignmentId = startAssignmentId;
+    internal static int NextAssignmentId { get => nextAssignmentId++; }
+
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+    internal static void reset()
+    {
+        nextAssignmentId = startAssignmentId;
+
+        Clock = DateTime.Now;
+    }
 }
