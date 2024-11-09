1ecdfd02a95d4b001ddf5eba4e9d66bd482ace22
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-09 23:00:45 +0200

    Proper capitaltion of the variables has been done

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -4,15 +4,15 @@ namespace Dal;
 internal static class Config
 {
     //Id generator for the Call model
-    internal const int startCallId = 1;
-    private static int nextCallId = startCallId;
-    internal static int NextCallId { get => nextCallId++; }
+    internal const int StartCallId = 1;
+    private static int s_nextCallId = StartCallId;
+    internal static int NextCallId { get => s_nextCallId++; }
 
 
     //Id generator for the Assignment model
-    internal const int startAssignmentId = 0;
-    private static int nextAssignmentId = startAssignmentId;
-    internal static int NextAssignmentId { get => nextAssignmentId++; }
+    internal const int StartAssignmentId = 0;
+    private static int s_nextAssignmentId = StartAssignmentId;
+    internal static int NextAssignmentId { get => s_nextAssignmentId++; }
 
     //RiskRange handler
     internal static TimeSpan RiskRange { get; set; } = TimeSpan.Zero;
@@ -20,8 +20,8 @@ internal static class Config
 
     internal static void reset()
     {
-        nextAssignmentId = startAssignmentId;
-        nextCallId = startCallId;
+        s_nextAssignmentId = StartAssignmentId;
+        s_nextCallId = StartCallId;
         RiskRange = TimeSpan.Zero;
         Clock = DateTime.Now;
     }
