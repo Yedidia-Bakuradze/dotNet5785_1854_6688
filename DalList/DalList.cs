c0b33c6bf84d821d0a99f76c366506423a95c2b3
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-09 23:25:35 +0200

    DalList class has been added including lists for all the types that were implemented in the project

---
+++ DalList/DalList.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Dal;
+
+/// <summary>
+/// List containing list of all types 
+/// </summary>
+internal static class DalList
+{
+    internal static List<DO.Assignment> Assignments { get;}= new(); 
+    internal static List<DO.Call> Calls {get;}= new();
+    internal static List<DO.Volunteer> Volunteers { get; } = new();
+}
