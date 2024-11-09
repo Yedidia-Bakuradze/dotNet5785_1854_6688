52c28f44da4eb7dd27f4ab57a0f495de9dbf29f3
Meir Crombie <meircrombie@gmail.com>
2024-11-09 23:31:40 +0200

    Add the "CallImplementation" class

---
+++ DalList/CallImplementation.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+public class CallImplementation : ICall
+{
+    public void Create(Call item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Call? Read(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<Call> ReadAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update(Call item)
+    {
+        throw new System.NotImplementedException();
+    }
+}       
+{
+}
