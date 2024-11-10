fc56f376a6eff7a4f641672f2d499305596948ab
Meir Crombie <meircrombie@gmail.com>
2024-11-10 19:53:04 +0200

    Add the CRUD methods of the Call Class

--- DalList/CallImplementation.cs
+++ DalList/CallImplementation.cs
@@ -6,32 +6,61 @@ public class CallImplementation : ICall
 {
     public void Create(Call item)
     {
-        throw new System.NotImplementedException();
+        int id = Config.NextCallId;
+        DalList.Calls.Add(item with { Id = id });
     }
 
     public void Delete(int id)
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            Call result = DalList.Calls.Find((call) => call.Id == id) ?? throw new Exception($"no such Call with id:{id}");
+            DalList.Calls.Remove(result);
+        }
+        catch (Exception errorMsg)
+        {
+            Console.WriteLine(errorMsg.Message);
+        }
     }
 
     public void DeleteAll()
     {
-        throw new System.NotImplementedException();
+        DalList.Calls.Clear();
     }
 
     public Call? Read(int id)
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            Call res = DalList.Calls.Find((call) => call.Id == id) ?? throw new Exception($"no such Call with id:{id}");
+            return res;
+        }
+        catch (Exception errorMsg)
+        {
+            Console.WriteLine(errorMsg.Message);
+            return null;
+        }
+
+
     }
 
     public List<Call> ReadAll()
     {
-        throw new System.NotImplementedException();
+        return new List<Call>(DalList.Calls);
     }
 
     public void Update(Call item)
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            Call result = DalList.Calls.Find((call) => call.Id == item.Id) ?? throw new Exception($"no such Call with id:{item.Id}");
+            Delete(result.Id);    
+            DalList.Calls.Add(item);
+        }
+        catch (Exception errorMsg)
+        {
+            Console.WriteLine(errorMsg.Message);
+        }
     }
 }       
 
