d30560e6039af3400183446b680722e273d8dc79
Meir Crombie <meircrombie@gmail.com>
2024-11-20 18:01:41 +0200

    IDal with all of his propertys has been added

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DalApi
+{
+    public interface IDal
+    {
+        IAssignment Assignment { get; }
+        ICall Call { get; }
+        IVolunteer Volunteer { get; }
+        IConfig Config { get; }
+
+        void ResetDB(); //Resets the database
+    }
+}
