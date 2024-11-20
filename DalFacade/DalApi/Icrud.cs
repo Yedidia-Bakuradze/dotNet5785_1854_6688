aac2dfc131ba3d3478c3e04f04ae25a97e7d0bbb
Meir Crombie <meircrombie@gmail.com>
2024-11-20 17:57:16 +0200

    All the interfaces of the entity are now inherited from ICrud

--- DalFacade/DalApi/Icrud.cs
+++ DalFacade/DalApi/Icrud.cs
@@ -2,11 +2,11 @@
 {
     public interface ICrud<T> where T : class
     {
-        void Create(T item); //Creates new entity object in DAL
-        T? Read(int id); //Reads entity object by its ID 
-        List<T> ReadAll(); //stage 1 only, Reads all entity objects
-        void Update(T item); //Updates entity object
-        void Delete(int id); //Deletes an object by its Id
-        void DeleteAll(); //Delete all entity objects
+        public void Create(T item); //Creates new entity object in DAL
+        public T? Read(int id); //Reads entity object by its ID 
+        public List<T> ReadAll(); //stage 1 only, Reads all entity objects
+        public void Update(T item); //Updates entity object
+        public void Delete(int id); //Deletes an object by its Id
+        public void DeleteAll(); //Delete all entity objects
     }
 }
