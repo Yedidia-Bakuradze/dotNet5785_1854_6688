e8b8f7b3ccc1f0a8762208d544b0e9126a360ed7
Meir Crombie <meircrombie@gmail.com>
2024-11-26 19:27:29 +0200

    Config Class has been pushed

---
+++ DalXml/Config.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Dal;
+
+static internal class Config
+{
+    internal const string dataConfigFileName = "data-config";
+    internal const string assignmentFileName = "assignments";
+    internal const string callFileName = "calls";
+    internal const string volunteerFileName = "volunteers";
+
+    internal static DateTime Clock { 
+        get =>  XMLTools.GetConfigDateVal(dataConfigFileName, "Clock");
+        set => XMLTools.SetConfigDateVal(dataConfigFileName, "Clock", value);
+    }
+
+    internal static int NextAssignmentId
+    {
+        get => XMLTools.GetAndIncreaseConfigIntVal(assignmentFileName, "NextAssignmentId");
+        private set => XMLTools.SetConfigIntVal(assignmentFileName, "NextAssignmentId", value);
+    }
+    internal static int NextCallId
+    {
+        get => XMLTools.GetAndIncreaseConfigIntVal(callFileName, "NextCallId");
+        private set => XMLTools.SetConfigIntVal(callFileName, "NextCallId", value);
+    }
+    // TODO: RiskRange handler
+    //internal static TimeSpan RiskRange
+    //{
+    //}
+
+
+    internal static void Reset()
+    {
+        NextAssignmentId = 1;
+        NextCallId = 1;
+        Clock = DateTime.Now;   
+        // RiskRange = TimeSpan.Zero;
+    }
+}
