5023a7dca743d896832da7b14769380ca6df81ba
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-11-26 19:32:35 +0200

    The implementation of the IConfig interface in the DalXml has been added

---
+++ DalXml/ConfigImplementation.cs
@@ -0,0 +1,21 @@
+﻿using DalApi;
+namespace Dal;
+
+internal class ConfigImplementation : IConfig
+{
+    public TimeSpan RiskRange // Inherits from IConfig
+    {
+        get => Config.RiskRange;
+        set => Config.RiskRange = value;
+    }
+    public DateTime Clock  // Inherits from IConfig
+    {
+        get => Config.Clock;
+        set => Config.Clock = value;
+    }
+
+    public void Reset() // Inherits from IConfig
+    {
+        Config.Reset();
+    }
+}
