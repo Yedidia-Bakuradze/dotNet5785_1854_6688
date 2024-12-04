a6d506acbd1dab4ea06d3798b7a8e9d53d2a00a2
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-04 23:07:12 +0200

    Factory class has been added to serve DalXml or DalList as the Factory design pattern requests to

---
+++ DalFacade/DalApi/Factory.cs
@@ -0,0 +1,34 @@
+﻿namespace DalApi;
+public static class Factory
+{
+    public static IDal Get
+    {
+        get
+        {
+            string dalType = DalApi.DalConfig.s_dalName
+                ?? throw new DalConfigException($"DAL name is not extracted from the configuration");
+            
+            DalApi.DalConfig.DalImplementation dal = DalApi.DalConfig.s_dalPackages[dalType]
+                ?? throw new DalConfigException($"Package for {dalType} is not found in packages list in dal-config.xml");
+
+            try {
+                System.Reflection.Assembly.Load(dal.Package ??
+                    throw new DalConfigException($"Package {dal.Package} is null"));
+            }
+            catch (Exception ex)
+            {
+                throw new DalConfigException($"Failed to load {dal.Package}.dll package", ex);
+            }
+
+            //Getting the data type based on the values in the JSON file - Eithter DalXml or DalList
+            Type type = Type.GetType($"{dal.Namespace}.{dal.Class}, {dal.Package}")
+                ??
+                throw new DalConfigException($"Class {dal.Namespace}.{dal.Class} was not found in {dal.Package}.dll");
+
+            //Accessing its static property and returning the Instance value
+            return type.GetProperty("Instance", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static)?.GetValue(null) as IDal
+                ??
+                throw new DalConfigException($"Class {dal.Class} is not a singleton or wrong property name for Instance");
+        }
+    }
+}
