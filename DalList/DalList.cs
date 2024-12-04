74907c66d94a2302766421b0493a2c6ed3a2b603
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-04 22:42:52 +0200

    Singleton design pattern (lazy and thread safe) has been implemented in the DalList class

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -1,8 +1,34 @@
 ﻿
 namespace Dal;
 using DalApi;
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    // A private instance which is initialized only when the DalList has been called at least one time (Lazy Singleton)
+    private static DalList instance = null!;
+    
+    //an indicator for a lcoa
+    private static readonly object lockObj = new object()!;
+    
+    //private ctor for class's use only
+    private DalList(){}
+
+    //Returns the same instance without craeating multiple instances (Singleton)
+    public static DalList Instance {
+        get { 
+            if(instance == null)
+            {
+                //Thread safe: Only one thread creates the instance
+                lock(lockObj){
+                    if(instance == null)
+                    {
+                        instance = new DalList();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+    
     public IAssignment Assignment { get; } = new AssignmentImplementation();
 
     public ICall Call { get; } = new CallImplementation();
