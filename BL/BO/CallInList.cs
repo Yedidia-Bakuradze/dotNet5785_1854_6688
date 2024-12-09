b1b756b0764558efd1f2789d4897ae4d85dd9cb0
Meir Crombie <meircrombie@gmail.com>
2024-12-09 22:29:18 +0200

    CallInList logic entity has been added

---
+++ BL/BO/CallInList.cs
@@ -0,0 +1,35 @@
+﻿
+namespace BO;
+
+public class CallInList
+{
+    // The unique identifier of the call in the list
+    public int? Id { get; init; }
+
+    // The identifier of the call
+    public int CallId { get; init; }
+
+    // The type of the call
+    public CallType TypeOfCall { get; set; }
+
+    // The opening time of the call
+    public DateTime OpenningTime { get; set; }
+
+    // The time remaining until the call ends
+    public TimeSpan? TimeToEnd { get; set; }
+
+    // The name of the last volunteer assigned to the call
+    public string? LastVolunteerName { get; set; }
+
+    // The elapsed time since the call was opened
+    public TimeSpan? TimeElapsed { get; set; }
+
+    // The status of the call
+    public CallStatus Status { get; set; }
+
+    // The total number of allocations made for the call
+    public int TotalAlocations { get; set; }
+
+    // Returns a string representation of the CallInList object
+    public override string ToString() => this.ToStringProperty();
+}
