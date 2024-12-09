72058f15018a9452e5d7266ff975306f3f7b0e14
Meir Crombie <meircrombie@gmail.com>
2024-12-09 23:58:27 +0200

    Icall has been added

---
+++ BL/BlApi/ICall.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BlApi;
+
+public interface ICall
+{
+    int GetTotalCallsByStatus();
+    IEnumerable<BO.CallInList> GetListOfCalls(BO.CallInList? parameter = null,object? value = null, BO.CallInList? parameter1 = null);
+    IEnumerable<BO.Call> GetDetialsOfCall(int callId);
+    void Update(BO.Call call);
+    void Delete(int id);
+
+}
