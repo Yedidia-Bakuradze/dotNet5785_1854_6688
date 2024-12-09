b295f132f208207de4ba4218ca661c9278280099
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-09 21:52:09 +0200

    The CallInProgress class has been implemented

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -4,3 +4,4 @@ public enum Roles  { Admin,Volunteer,Undefined };
 public enum TypeOfRange { AirDistance, WalkingDistance, DrivingDistance};
 public enum TypeOfEnding { Treated, SelfCanceled, AdminCanceled, CancellationExpired, Undefined };
 public enum CallTypes {Undefined,FoodPreparation,FoodDelivery}
+
