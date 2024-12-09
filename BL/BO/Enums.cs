a81485ef02770a30d1b47bc236e225716851a256
Meir Crombie <meircrombie@gmail.com>
2024-12-09 21:58:13 +0200

    Add the VolunteerInList BL logic

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -2,4 +2,4 @@
 public enum Roles { Admin, Volunteer, Undefined };
 public enum TypeOfRange { AirDistance, WalkingDistance, DrivingDistance };
 public enum TypeOfEnding { Treated, SelfCanceled, AdminCanceled, CancellationExpired, Undefined };
-public enum CallTypes { Undefined, FoodPreparation, FoodDelivery }
+public enum CallTypes { None ,Undefined, FoodPreparation, FoodDelivery }
