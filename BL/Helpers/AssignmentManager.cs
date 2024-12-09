f7e6f8245818e9723c4b78b3b1f44dbf07c787ce
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-09 23:32:14 +0200

    AssignmentManager and VolunteerManager help classes been implemented

---
+++ BL/Helpers/AssignmentManager.cs
@@ -0,0 +1,10 @@
+﻿using DalApi;
+
+namespace Helpers;
+
+//Remember: All the method shall be as internal static
+internal static class AssignmentManager
+{
+    private static IDal s_dal = Factory.Get; //stage 4
+
+}
