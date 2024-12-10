0f8e9db734eb0a74c832b3b3f6a3c9f1e088e5de
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-10 17:53:59 +0200

    Exceptions been added to the BL layer to handle internal exceptions and exceptions which been thrown from the Dal layer

---
+++ BL/BO/Exceptions.cs
@@ -0,0 +1,34 @@
+﻿namespace BO;
+
+#region DalBLException
+[Serializable]
+public class BoDoesNotExistException : Exception
+{
+    public BoDoesNotExistException(string? msg,DO.DalDoesNotExistException ex) : base(msg) { }
+}
+
+[Serializable]
+public class BoAlreadyExistsException : Exception
+{
+    public BoAlreadyExistsException(string? msg,DO.DalAlreadyExistsException ex) : base(msg) { }
+}
+
+#endregion DalBLException
+
+
+#region BL
+[Serializable]
+public class BoInvalidEntityDetails : Exception
+{
+    public BoInvalidEntityDetails(string? msg) : base(msg) { }
+}
+
+[Serializable]
+public class BoInvalidEntityFieldFormatting : Exception
+{
+    public BoInvalidEntityFieldFormatting(string? msg) : base(msg) { }
+}
+
+
+
+#endregion BL
