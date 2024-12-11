862e23b60e22dd023af6e7425e94f9f3e49a8071
Meir Crombie <meircrombie@gmail.com>
2024-12-11 13:57:49 +0200

    UpdateAllOpenAndExpierdCalls has been done

--- BL/Helpers/CallManager.cs
+++ BL/Helpers/CallManager.cs
@@ -1,11 +1,41 @@
-﻿
-
-using DalApi;
-
+﻿using DalApi;
+using DO;
+using System.Security.Cryptography;
 namespace Helpers;
 
 internal static class CallManager
 {
     private static IDal s_dal = Factory.Get; //stage 4
+    public static void UpdateAllOpenAndExpierdCalls()
+    {
+        List<DO.Call> listOfCalls = s_dal.Call.ReadAll((DO.Call call) => call.DeadLine < ClockManager.Now).ToList();
+        List<DO.Assignment> listOfAssignments = s_dal.Assignment.ReadAll().ToList();
 
+        listOfCalls
+            .Where(call => !listOfAssignments.Any(assignment => assignment.CallId == call.Id))
+            .Select(call => call)
+            .ToList()
+            .ForEach(call => s_dal.Assignment.Create( new DO.Assignment
+        {
+            CallId = call.Id,
+            TimeOfStarting = call.OpeningTime,
+            TimeOfEnding = ClockManager.Now,
+            Id = -1,
+            TypeOfEnding = DO.TypeOfEnding.CancellationExpired,
+            VolunteerId = -1,//The instructions asked us to put NULL here, but the project instructions themselves state that it should not be NULL, so we made it -1 because it makes the most sense for us
+        }
+        ));
+
+        listOfAssignments
+            .Where(assignment => listOfCalls.Any(call => call.Id == assignment.CallId))
+            .Select(assignment => assignment)
+            .ToList()
+            .ForEach((assignment) => s_dal.Assignment.Update(assignment with
+        {
+            TimeOfEnding = ClockManager.Now,
+            TypeOfEnding = DO.TypeOfEnding.CancellationExpired
+        }));
+
+
+    }
 }
