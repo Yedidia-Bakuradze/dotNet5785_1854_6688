92a617577b6f69917c8da4964b94f2f8f7e28dc2
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-12 17:56:52 +0200

    The main program has been initialized

--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        //Main BL Action manager for the bl layer
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+
+        public void MainMenu()
+        {
+            Console.WriteLine(@"
+----------------------------------------------------------------
+Select Your Option:
+
+Press 1 To Use ICall Interface
+Press 2 To Use IVolunteer Interface
+Press 3 To Use IAdmin Interface
+Press 0 To Exit
+----------------------------------------------------------------
+");
         }
     }
 }
