df4638284c6f670f835157230ffb4c1391b8c629
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-12 10:42:46 +0200

    The Bl class which manages access to use all the interfaces and implememtation in the BL layer has been defined and added

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,12 @@
+﻿namespace BlImplementation;
+using BlApi;
+
+/// <summary>
+/// This class managers all the BL optional actions using the defined interfaces 
+/// </summary>
+internal class Bl : IBl
+{
+    public ICall Call { get; } = new CallImplementation();
+    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+    public IAdmin Admin { get; } = new AdminImplementation();
+}
