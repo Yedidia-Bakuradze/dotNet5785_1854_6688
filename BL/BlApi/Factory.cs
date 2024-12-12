d45f870b526b771e0c7ebe6d3824cf1e2816cd60
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-12 12:05:44 +0200

    The Factory for the BL layer (used in PL layer) has been implemented

---
+++ BL/BlApi/Factory.cs
@@ -0,0 +1,11 @@
+﻿namespace BlApi;
+
+// BL managment instance Factory
+public static class Factory
+{
+    /// <summary>
+    /// This GET returns an managments instance of the BL layer for the PL layer
+    /// </summary>
+    /// <returns></returns>
+    public static IBl Get() => new BlImplementation.Bl();
+}
