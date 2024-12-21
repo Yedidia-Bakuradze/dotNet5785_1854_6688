e855d751735914d902cfc0a3291fcaa7f7c3fbcf
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-22 00:56:52 +0200

    The combo box values been filled with the proper values from the enum of Call Types

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -2,7 +2,7 @@
 
 namespace PL;
 
-internal class CallTypesCollection : IEnumerable
+internal class CallTypesCollection: IEnumerable
 {
     static readonly IEnumerable<BO.CallType> s_enums =
         (Enum.GetValues(typeof(BO.CallType)) as IEnumerable<BO.CallType>)!;
