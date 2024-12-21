318579089bbc5441440a21bdb478e418d3a5c52b
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-22 00:51:37 +0200

    The enum containing all the call types in our project has been added

---
+++ PL/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace PL;
+
+internal class CallTypesCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.CallType> s_enums =
+        (Enum.GetValues(typeof(BO.CallType)) as IEnumerable<BO.CallType>)!;
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
