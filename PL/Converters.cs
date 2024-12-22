d644541ed7d012dd7b509381e1d912615c01fab9
Meir Crombie <meircrombie@gmail.com>
2024-12-22 14:04:23 +0200

    ConvertIdToReadOnly has been added

---
+++ PL/Converters.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace PL;
+
+class ConvertIdToReadOnly : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return true;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+}
