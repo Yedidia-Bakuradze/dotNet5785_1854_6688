02f2104db00f20556e70b145dcc5155eb986fb94
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-22 14:13:38 +0200

    All the num fields have disabled for editing

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -3,15 +3,21 @@ using System.Windows.Data;
 
 namespace PL;
 
-class ConvertIdToReadOnly : IValueConverter
+/// <summary>
+/// Converts Int value into a boolean true value to disable the editing on this field
+/// </summary>
+class ConvertIntToReadOnlyTrue : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        return true;
-    }
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
-    }
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => true;
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 
+}
+
+/// <summary>
+/// Converts CallStatus value into a boolean true value to disable the editing on this field
+/// </summary>
+class ConvertStatusToReadOnlyTrue : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => true;
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
