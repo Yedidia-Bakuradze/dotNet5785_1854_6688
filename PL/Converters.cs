162843a1a0324dcc6e3e4cca350a18e19a83f4c8
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-23 14:55:25 +0200

    The show call in progress section in the Volunteer Window has been disabled when there is no call to show

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -51,3 +51,14 @@ class ConvertVolunteerEnditingModeToReadOnlyValue: IValueConverter
         return "Update Volunteer";
     }
 }
+
+class ConvertCallInProgressToVisibility : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return Visibility.Hidden;
+        return Visibility.Visible;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
