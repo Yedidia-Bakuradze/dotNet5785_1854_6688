2935416b6fbba484d62722a6df2c713edb666133
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-13 17:35:10 +0200

    Code behind of the call window has been configured

---
+++ PL/Call/CallWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace PL.Call;
+
+public partial class CallWindow : Window
+{
+
+    public CallWindow(int callId)
+    {
+        CurrentCall = s_bl.Call.GetDetielsOfCall(callId);
+        InitializeComponent();
+    }
+    #region Dependecy Propeties
+    public BO.Call CurrentCall
+    {
+        get => (BO.Call)GetValue(CurrentCallPropety);
+        set => SetValue(CurrentCallPropety,value);
+    }
+
+    private static DependencyProperty CurrentCallPropety =
+        DependencyProperty.Register("CurrentCall", typeof(BO.Call), typeof(CallWindow));
+    #endregion
+
+    #region Regular Propeties
+    private static BlApi.IBl s_bl = BlApi.Factory.Get();
+
+    #endregion
+
+    #region Events
+    #endregion
+
+    #region Method
+    #endregion
+
+    private void OnSubmitBtnClick(object sender, RoutedEventArgs e) => MessageBox.Show("Unimplemened");
+}
