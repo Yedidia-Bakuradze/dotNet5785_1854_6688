adce56d9027e7e4ea5239ded94fd39640c1d00e1
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-14 18:25:53 +0200

    Single open call window has been added

---
+++ PL/Call/OpenCallWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace PL.Call;
+
+public partial class OpenCallWindow : Window
+{
+    private readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+    public OpenCallWindow(int callId)
+    {
+        try
+        {
+            CurrentCall = s_bl.Call.GetDetielsOfCall(callId);
+        }
+        catch(Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+            this.Close();
+        }
+        InitializeComponent();
+    }
+
+
+    public BO.Call CurrentCall 
+    {
+        get => (BO.Call)GetValue(CurrentCallProperty);
+        set => SetValue(CurrentCallProperty,value);
+    }
+
+    private static readonly DependencyProperty CurrentCallProperty
+        = DependencyProperty.Register("CurrentCall", typeof(BO.Call),typeof(OpenCallListWindow));
+}
