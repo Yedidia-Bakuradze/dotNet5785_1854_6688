dc49e49e10a39e7c0b79addf5ae813c94d093a21
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-15 17:22:59 +0200

    The volunteer details sub window has been added

---
+++ PL/Sub Windows/VolunteerDetailsControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL.Sub_Windows;
+
+public partial class VolunteerDetailsControl : UserControl
+{
+    public VolunteerDetailsControl(BO.Volunteer volunteer)
+    {
+        CurrentVolunteer = volunteer;
+        IsActive = volunteer.IsActive ? "Yes" : "No";
+        InitializeComponent();
+    }
+
+    public BO.Volunteer? CurrentVolunteer { get; set; }
+    public string Password { get; set; } = "******************";
+    public string IsActive { get; set; }
+}
