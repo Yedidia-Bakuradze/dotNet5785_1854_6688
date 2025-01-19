788da0bcb50ced53fa186bf16fc3d0de43413b73
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-19 20:32:51 +0200

    Filtering and sorting has been updated with dps

--- PL/Call/ClosedCallListWindow.xaml.cs
+++ PL/Call/ClosedCallListWindow.xaml.cs
@@ -12,8 +12,11 @@ public partial class ClosedCallListWindow : Window
         InitializeComponent();
     }
 
+    #region Regular Properties
     public int VolunteerId { get; set; }
+    #endregion
 
+    #region Dependency Properties
     public IEnumerable<BO.ClosedCallInList> ListOfCalls 
     {
         get => (IEnumerable<BO.ClosedCallInList>)GetValue(ListOfCallsProperty);
@@ -23,17 +26,40 @@ public partial class ClosedCallListWindow : Window
     private static readonly DependencyProperty ListOfCallsProperty =
         DependencyProperty.Register("ListOfCalls", typeof(IEnumerable<BO.ClosedCallInList>), typeof(ClosedCallListWindow));
 
-    public BO.CallType? FilterValue { get; set; }
-    public BO.ClosedCallInListFields? SortFiled { get; set; }
-
-    public BO.ClosedCallInList? SelectedCall { get; set; }
-
-    private void RefereshList() => ListOfCalls = s_bl.Call.GetClosedCallsByVolunteer(VolunteerId, FilterValue, SortFiled);
-
-    private void OnFilterValueChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    public BO.CallType? FilterValue
     {
-        RefereshList();
+        get { return (BO.CallType?)GetValue(FilterValueProperty); }
+        set { SetValue(FilterValueProperty, value); }
     }
+
+    // Using a DependencyProperty as the backing store for FilterValue.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty FilterValueProperty =
+        DependencyProperty.Register("FilterValue", typeof(BO.CallType?), typeof(ClosedCallListWindow));
+
+    public BO.ClosedCallInListFields? SortField
+    {
+        get { return (BO.ClosedCallInListFields?)GetValue(SortFieldProperty); }
+        set { SetValue(SortFieldProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for SortField.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty SortFieldProperty =
+        DependencyProperty.Register("SortField", typeof(BO.ClosedCallInListFields?), typeof(ClosedCallListWindow));
+
+    public BO.ClosedCallInList SelectedCall
+    {
+        get { return (BO.ClosedCallInList)GetValue(SelectedCallProperty); }
+        set { SetValue(SelectedCallProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for SelectedCall.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty SelectedCallProperty =
+        DependencyProperty.Register("SelectedCall", typeof(BO.ClosedCallInList), typeof(ClosedCallListWindow));
+    #endregion
+
+    #region Events
+
+    private void OnFilterValueChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) => RefereshList();
 
     private void OnSortingValueChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
     {
@@ -55,4 +81,17 @@ public partial class ClosedCallListWindow : Window
     {
         s_bl.Call.RemoveObserver(RefereshList);
     }
+    private void OnFliterAndSort(object sender, RoutedEventArgs e) => RefereshList();
+
+    private void OnResetParameters(object sender, RoutedEventArgs e)
+    {
+        FilterValue = null;
+        SortField = null;
+    }
+
+    #endregion
+
+    #region Methods
+    private void RefereshList() => ListOfCalls = s_bl.Call.GetClosedCallsByVolunteer(VolunteerId, FilterValue, SortField);
+    #endregion
 }
