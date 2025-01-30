b1a462bcfd047b887a97d0f2bb605354d233372a
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-01-30 23:44:17 +0200

    Open Call Window has been fixed to show the map (Issue #81)

--- PL/Call/OpenCallWindow.xaml.cs
+++ PL/Call/OpenCallWindow.xaml.cs
@@ -42,37 +42,30 @@ public partial class OpenCallWindow : Window
         get => (BO.Call)GetValue(CurrentCallProperty);
         set => SetValue(CurrentCallProperty,value);
     }
-
     private static readonly DependencyProperty CurrentCallProperty
-        = DependencyProperty.Register("CurrentCall", typeof(BO.Call),typeof(OpenCallListWindow));
-
-
+        = DependencyProperty.Register("CurrentCall", typeof(BO.Call),typeof(OpenCallWindow));
 
     public BO.Volunteer CurrentVolunteer
     {
         get { return (BO.Volunteer)GetValue(CurrentVolunteerProperty); }
         set { SetValue(CurrentVolunteerProperty, value); }
     }
-
-    // Using a DependencyProperty as the backing store for CurrentVolunteer.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty CurrentVolunteerProperty =
-        DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(OpenCallListWindow));
-
-
+        DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(OpenCallWindow));
+    
     public UserControl MapView
     {
         get { return (UserControl)GetValue(MapViewProperty);}
         set { SetValue(MapViewProperty, value); }
     }
-
     public static readonly DependencyProperty MapViewProperty =
-        DependencyProperty.Register("MapView", typeof(UserControl), typeof(OpenCallListWindow));
+        DependencyProperty.Register("MapView", typeof(UserControl), typeof(OpenCallWindow));
 
     #endregion
 
     #region Methods
-    private void Observer() => ReloadScreen();
-    private async void ReloadScreen()
+    private void Observer() => _ = ReloadScreen();
+    private async Task ReloadScreen()
     {
         try
         {
@@ -85,27 +78,23 @@ public partial class OpenCallWindow : Window
             Close();
         }
 
-        if ((CurrentCall.Latitude, CurrentCall.Longitude) is not (null,null) && (_observerOperation is null || _observerOperation.Status == DispatcherOperationStatus.Completed))
-        {
-            _observerOperation = Dispatcher.BeginInvoke(() =>
-            {
 
-                    List<(double, double)> listOfCordinates = new();
-                    listOfCordinates.Add(((double)CurrentCall.Latitude!, (double)CurrentCall.Longitude!));
-
-                    if ((CurrentVolunteer.Latitude,CurrentVolunteer.Longitude) is not (null,null))
-                    {
-                        listOfCordinates.Insert(0, ((double)CurrentVolunteer.Latitude!, (double)CurrentVolunteer.Longitude!));
-                        MapView = new DisplayMapContent(TypeOfMap.MultipleTypeOfRoutes, BO.TypeOfRange.WalkingDistance, listOfCordinates);
-                    }
-                    else
-                    {
-                        MapView = new DisplayMapContent(TypeOfMap.Pin, BO.TypeOfRange.WalkingDistance, listOfCordinates);
-                    }
-
-            });
-        }
+        if((CurrentCall.Latitude, CurrentCall.Longitude) is (null,null))
+            return;
         
+        List<(double, double)> listOfCordinates = [((double)CurrentCall.Latitude!, (double)CurrentCall.Longitude!)];
+
+        if ((CurrentVolunteer.Latitude,CurrentVolunteer.Longitude) is not (null,null))
+        {
+            listOfCordinates.Insert(0, ((double)CurrentVolunteer.Latitude!, (double)CurrentVolunteer.Longitude!));
+            MapView = new DisplayMapContent(TypeOfMap.MultipleTypeOfRoutes, BO.TypeOfRange.WalkingDistance, listOfCordinates);
+        }
+        else
+        {
+            MapView = new DisplayMapContent(TypeOfMap.Pin, BO.TypeOfRange.WalkingDistance, listOfCordinates);
+        }
+
+
     }
     #endregion
 
